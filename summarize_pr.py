1c9e507e3021859b17e862366848e06eba9b50e2
suman sah <shahsuman438@gmail.com>
2025-02-02 23:46:59 +0545

    code refactor

--- summarize_pr.py
+++ summarize_pr.py
@@ -15,7 +15,7 @@ open_ai_key = os.getenv("OPEN_AI_KEY")
 client = OpenAI(api_key=open_ai_key)
 
 
-def get_completion(prompt, model="gpt-4o-mini", temperature=1):
+def feed_and_get_content(prompt, model="gpt-4o-mini", temperature=1):
     messages = [{"role": "user", "content": prompt}]
     response = client.chat.completions.create(
         model=model,
@@ -30,11 +30,12 @@ Generate the summarized PR Description with the following code mention in triple
 with key title:'Generate suatable PR Title' and body:'this should be markup text includes topics like summary(summarize the changes), 
 changes(short description for each code file and code changes),screenshots('leave this blank just have heading') ,
 suggestion(suggest the improvements also include the code improvement), 
-possible breakage(check for possible breakage of the code if there is then point out that code and suggestion to fix)', 
+possible breakage(check for possible breakage of the code if there is then point out that code and suggestion to fix)',
+ 
 ```{code_diff}```
 """
 
-response = get_completion(prompt)
+response = feed_and_get_content(prompt)
 cleaned_response = response.replace("`", "").replace("json", "")
 try:
     responseJson = json.loads(cleaned_response) if isinstance(cleaned_response, str) else cleaned_response
